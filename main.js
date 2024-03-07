a432c778a9549c692fbf6afe7e328c80470dacd4
Pon4C <camtuyl@gmail.com>
2024-03-07 11:29:30 +0700

    TH1

---
+++ main.js
@@ -0,0 +1,458 @@
+const exam = {
+    'part1': {
+        1: {
+            'topic': "1+1=2",
+            'correctAnswer': '1',
+        },
+        2: {
+            'topic': "1+2=3",
+            'correctAnswer': ''
+        },
+        3: {
+            'topic': "1+3=4",
+            'correctAnswer': ''
+        },
+        
+        4: {
+            'topic': "1+4=5",
+            'correctAnswer': ''
+        },
+        5: {
+            'topic': "1+5=6",
+            'correctAnswer': ''
+        }, 
+        6: {
+            'topic': "1+6=7",
+            'correctAnswer': ''
+        },
+        7: {
+            'topic': "1+7=8",
+            'correctAnswer': ''
+        },
+        8: {
+            'topic': "1+8=9",
+            'correctAnswer': ''
+        }, 
+        9: {
+            'topic': "1+9=10",
+            'correctAnswer': ''
+        },
+        10: {
+            'topic': "1+0=1",
+            'correctAnswer': ''
+        }
+    }, 
+    'part2': {
+        11: {
+            'topic': "Nước nào dưới đây không thuộc trong 11 nước thành lập khối NATO năm 1949?",
+            'answerA': "Bồ Đào Nha",
+            'answerB': "Anh",
+            'answerC': "Cộng hòa Liên bang Đức.",
+            'answerD': "Hà Lan",
+            'correctAnswer': "C"
+        },
+        12: {
+            'topic': "Sự phân chia đối lập về kinh tế và chính trị giữa các nước Tây Âu tư bản chủ nghĩa và Đông Âu xã hội chủ nghĩa được tạo nên bởi",
+            'answerA': "học thuyết Ních-xơn của Mĩ.",
+            'answerB': "việc thực hiện Kế hoạch Mác-san",
+            'answerC': "sự ra đời của khối Hiệp ước Vác-sa-va.",
+            'answerD': "sự thành lập khối quân sự NATO.",
+            'correctAnswer': "B"
+        },
+        13: {
+            'topic': "Liên minh chính trị - quân sự mang tính chất phòng thủ của các nước xã hội chủ nghĩa châu Âu là",
+            'answerA': "Tổ chức Hiệp ước Vácsava",
+            'answerB': "Tổ chức Hiệp ước Bắc Đại Tây Dương",
+            'answerC': "Liên hợp quốc",
+            'answerD': " Liên minh châu Âu",
+            'correctAnswer': "A"
+        },
+        14: {
+            'topic': "Trong cuộc gặp gỡ không chính thức ở Manta (Địa Trung Hải, 12/1989), Tổng Bí thư Đảng Cộng sản Liên Xô và Tổng thống Mĩ đã cùng tuyên bố ",
+            'answerA': "chấm dứt việc chạy đua vũ trang",
+            'answerB': "hạn chế vũ khí hạt nhân hủy diệt",
+            'answerC': "chấm dứt Chiến tranh lạnh",
+            'answerD': "giữ gìn hòa bình, an ninh cho nhân loại.",
+            'correctAnswer': "A"
+        }
+        ,15: {
+            'topic': "Mĩ đề ra Kế hoạch Mác-san (6/1947) nhằm",
+            'answerA': "viện trợ kinh tế cho Liên Xô và các nước châu Âu.",
+            'answerB': "tập hợp các nước Tây Âu vào liên minh quân sự chống Liên Xô và Đông Âu",
+            'answerC': "mở đầu cuộc Chiến tranh lạnh với Liên Xô.",
+            'answerD': "thành lập liên minh phòng thủ của các nước tư bản ở khu vực châu Âu.",
+            'correctAnswer': "A"
+        },
+        16: {
+            'topic': "Sau khi cách mạng Trung Quốc thành công (10 - 1949) cuộc kháng chiến của nhân dân Việt Nam có những điều kiện thuận lợi gì?",
+            'answerA': "Được sự ủng hộ và giúp đỡ của Trung Quốc",
+            'answerB': "Được sự ủng hộ và giúp đỡ của Trung Quốc, Liên xô và các nước xã hội chủ nghĩa.",
+            'answerC': "Được sự ủng hộ và giúp đỡ của Trung Quốc và các nước châu Á.",
+            'answerD': "Buộc thực dân Pháp phải chấm dứt chiến tranh xâm lược ở Việt Nam.",
+            'correctAnswer': "A"
+        },
+        17: {
+            'topic': "Thực dân Pháp buộc phải công nhận độc lập chủ quyền, thống nhất và toàn vẹn lãnh thổ của 3 nước Đông Dương vào thời gian nào ?",
+            'answerA': "1954",
+            'answerB': "1951",
+            'answerC': "1950",
+            'answerD': "1945",
+            'correctAnswer': "A"
+        },
+        18: {
+            'topic': " Hiệp định về những cơ sở của quan hệ Đông Đức và Tây Đức được kí kết vào năm",
+            'answerA': "1970",
+            'answerB': "1971",
+            'answerC': "1972",
+            'answerD': "1973",
+            'correctAnswer': "A"
+        },
+        19: {
+            'topic': "Đầu tháng 8 - 1975, 33 nước châu Âu cùng với những nước nào ký kết Định ước Henxinki?",
+            'answerA': "Mĩ và Liên Xô",
+            'answerB': "Mĩ và Pháp",
+            'answerC': "Mĩ và Canađa",
+            'answerD': "Mĩ và Anh.",
+            'correctAnswer': "A"
+        },
+        20: {
+            'topic': "Trong cuộc gặp gỡ không chính thức ở Manta (Địa Trung Hải, 12/1989), Tổng Bí thư Đảng Cộng sản Liên Xô và Tổng thống Mĩ đã cùng tuyên bố ",
+            'answerA': "chấm dứt việc chạy đua vũ trang",
+            'answerB': "hạn chế vũ khí hạt nhân hủy diệt",
+            'answerC': "chấm dứt Chiến tranh lạnh",
+            'answerD': "giữ gìn hòa bình, an ninh cho nhân loại.",
+            'correctAnswer': "A"
+    }
+  
+    }, 
+    'part3': {
+        21: {
+            'topic': "Khái niệm nào là đúng về Sử học?",
+            'answerA': "Sử học là khoa học nghiên cứu về hoạt động của con người",
+            'answerB': "Sử học là khoa học nghiên cứu về văn hóa của con người",
+            'answerC': "Sử học là khoa học nghiên cứu về quá khứ của con người",
+            'answerD': "Sử học là khoa học nghiên cứu về tiến hóa của con người.",
+            'correctAnswer': ['A', "B", "C"]
+        },
+        22: {
+            'topic': "Nhận thức lịch sử phụ thuộc vào yếu tố nào sau đây của người tìm hiểuLịch sử?",
+            'answerA': "Nội dung tiến hành nghiên cứu",
+            'answerB': "Phương pháp điều tra ngoài thực địa",
+            'answerC': "Điều kiện",
+            'answerD': "phương pháp tìm hiểu",
+            'correctAnswer': ['A', "B", "C"]
+        },
+        23: {
+            'topic': " Nội dung nào sau đây là phương pháp cơ bản của Sử học?",
+            'answerA': "Lịch sử, lô-gich",
+            'answerB': "đồng đại,lịch đại",
+            'answerC': "liên ngành",
+            'answerD': "Khảo cứu",
+            'correctAnswer': ['A', "B", "C"]
+        },
+        24: {
+            'topic': " Nội dung nào sau đây là nhiệm vụ của Sử học?",
+            'answerA': "Nhận thức",
+            'answerB': "Giáo dục",
+            'answerC': "Dự báo",
+            'answerD': "Nghiên cứu",
+            'correctAnswer': ['A', "B", "C"]
+        },
+        25: {
+            'topic': "Chất lượng cuộc sống là:",
+            'answerA': "Khái niệm phản ánh độ được đáp ứng những nhu cầu cơ bản của con người về vật chất, tinh thần và chất lương môi trường.",
+            'answerB': "Sự phản ánh tuổi thọ trung bình của dân cư",
+            'answerC': "Sự phản ánh mức độ học vấn của người dân",
+            'answerD': " Sự phản ánh mức độ sống của người dân",
+            'correctAnswer': ['A', "B", "C"]
+        },
+        26: {
+            'topic': "Nơi thu nhập bình quân đầu người cao nhất nước ta là:",
+            'answerA': "tỷ lệ sinh cao",
+            'answerB': "số người nhập cư nhiều",
+            'answerC': "dân số tăng quá nhanh",
+            'answerD': "tuổi thọ trung bình cao",
+            'correctAnswer': ['A', "B", "C"]
+        },
+        27: {
+            'topic': "Để đảm bảo công bằng xã hội, văn minh, đảm bảo sự phát triển các nguồn lực con người và phát triển bền vững về kinh tế xã hội thì vấn đề cấp thiết đặt ra với nước ta hiện nay là:",
+            'answerA': "tỷ lệ sinh cao",
+            'answerB': "số người nhập cư nhiều",
+            'answerC': "dân số tăng quá nhanh",
+            'answerD': "tuổi thọ trung bình cao",
+            'correctAnswer': ['A', "B", "C"]
+        },
+        28: {
+            'topic': "Quá trình nào sau đây là xử lí thông tin sử liệu?",
+            'answerA': "Phân loại",
+            'answerB': "Phân tích",
+            'answerC': "Sưu tầm",
+            'answerD': "Tìm kiếm",
+            'correctAnswer': ['A', "B", "C"]
+        },
+        29: {
+            'topic': " Nội dung nào sau đây là nhiệm vụ của Sử học?",
+            'answerA': "Nhận thức",
+            'answerB': "Giáo dục",
+            'answerC': "Dự báo",
+            'answerD': "Nghiên cứu",
+            'correctAnswer': ['A', "B", "C"]
+        },
+        30: {
+            'topic': " Nội dung nào sau đây là nguyên tắc của Sử học?",
+            'answerA': "Khách quan",
+            'answerB': "Trung thực",
+            'answerC': "Nhân văn",
+            'answerD': "Chủ quan",
+            'correctAnswer': ['A', "B", "C"]
+        },
+  
+    },
+    'part4': {
+        31: {
+            'topic': "Giáo dục tư tưởng, tình cảm, đạo đức và rút ra bài học kinh nghiệm hiệntại là chức năng nào của sử học?",
+            'correctAnswer': "1"
+        },
+        32: {
+            'topic': "Sử học vừa phải phản ánh sự thật trong quá khứ, nhưng không kích động hận thù, xung đột hoặc kỳ thị, phân biệt đối xử. Đây là nguyên tắc nào của sửhọc?",
+            'correctAnswer': "1"
+        },
+        33: {
+            'topic': " Tìm hiểu về nguồn cội là nhu cầu nào của con người?",
+            'correctAnswer': "1"
+        },
+        34: {
+            'topic': "Nguyên nhân chính làm cho nước ta có điều kiện tiếp xúc với nền văn hóa thế giới là do:",
+            'correctAnswer': "1"
+        },
+        35: {
+            'topic': "Qua câu truyện cổ tích “Thánh Gióng” đánh đuổi giặc Ân. Hãy cho biết,đây thuộc loại nguồn sử liệu nào?",
+            'correctAnswer': "1"
+        },
+        36: {
+            'topic': "Qua câu truyện cổ tích “Thánh Gióng” đánh đuổi giặc Ân. Hãy cho biết,đây thuộc loại nguồn sử liệu nào?",
+            'correctAnswer': "1"
+        },
+        37: {
+            'topic': "Lịch sử cung cấp cho con người những thông tin hữu ích nào?",
+            'correctAnswer': "1"
+        },
+        38: {
+            'topic': "Lịch sử cung cấp cho con người những thông tin hữu ích nào?",
+            'correctAnswer': "1"
+        },
+        39: {
+            'topic': "Lịch sử cung cấp cho con người những thông tin hữu ích nào?",
+            'correctAnswer': "1"
+        },
+        40: {
+            'topic': "Qua câu truyện cổ tích “Thánh Gióng” đánh đuổi giặc Ân. Hãy cho biết,đây thuộc loại nguồn sử liệu nào?",
+            'correctAnswer': "1"
+        }
+  
+    }
+  }
+  
+  const userAnswer = {}
+  
+  const redirectPage = (newPage) => {
+    window.location.href = newPage
+  }
+  
+  const result = {
+    1: '1', 
+    2: '1',
+    3: '1',
+    4: '1',
+    5: '1',
+    7: '1', 
+    8: '1',
+    9: '1',
+    10: '1',
+    12: '1',
+    13: '1', 
+    14: '1',
+    15: '1',
+    16: '1',
+    17: '1',
+    18: '1',
+    19: '1',
+    20: '1',
+    21: '1',
+    22: '1',
+    23: '1', 
+    24: '1',
+    25: '1',
+    26: '1',
+    27: '1',
+    28: '1',
+    29: '1',
+    30: '1',
+    31: '1',
+    32: '1',
+    33: '1',
+    34: '1',
+    20: '1',
+    21: '1',
+    22: '1',
+    23: '1', 
+    24: '1',
+    25: '1',
+    26: '1',
+    27: '1',
+    28: '1',
+    29: '1',
+    30: '1',
+    31: '1',
+    32: '1',
+    33: '1',
+    34: '1',
+    35: '1',
+    36: '1',
+    37: '1',
+    38: '1',
+    39: '1',
+    40: '1',
+  }
+  
+  const loadExamPage = () => {
+    const {part1, part2, part3, part4} = exam
+    const examPart1 = document.getElementById("exam-part-1")
+    const examPart2 = document.getElementById("exam-part-2")
+    const examPart3 = document.getElementById('exam-part-3')
+    const examPart4 = document.getElementById("exam-part-4")
+  
+    Object.keys(part1).forEach((questionKey) => {
+        const question = part1[questionKey]
+        console.log(part1)
+        const {topic, correctAnswer} = question
+        const questionBox = 
+            `<div class="part-1-question">
+                <div class="topic part-1-topic">Câu ${questionKey}: ${topic}</div>
+                <div class="part-1-answers">
+                    <div class="part-1-answer">
+                        <input value="1" type="radio" name="part-1-answer-${questionKey}" onclick="onChangeAnswerPart2(${questionKey}, '1')">
+                        Đúng
+                    </div>
+                    <div class="part-1-answer">
+                        <input value="0" type="radio" name="part-1-answer-${questionKey}" onclick="onChangeAnswerPart2(${questionKey}, '0')">
+                        Sai
+                    </div>
+                </div>
+            </div>`
+        examPart1.innerHTML += questionBox
+  
+        userAnswer[questionKey] = ''
+    })
+  
+    Object.keys(part2).forEach((questionKey) => {
+        const question = part2[questionKey]
+        const {topic, answerA, answerB, answerC, answerD, correctAnswer} = question
+        const questionBox = 
+            `<div class="part-1-question">
+                <div class="part-1-topic">Câu ${questionKey}: ${topic}</div>
+                <div class="part-1-answers">
+                    <div class="part-1-answer">
+                        <input type="radio" name="part-2-answer-${questionKey} value="A" onclick="onChangeAnswerPart2(${questionKey}, 'A')">
+                        A. ${answerA}.
+                    </div>
+                    <div class="part-1-answer">
+                        <input type="radio" name="part-2-answer-${questionKey} value="B"  onclick="onChangeAnswerPart2(${questionKey}, 'B')">
+                        B.  ${answerB}
+                    </div>
+                    <div class="part-1-answer">
+                        <input type="radio" name="part-2-answer-${questionKey} value="C"  onclick="onChangeAnswerPart2(${questionKey}, 'C')">
+                        C.  ${answerC}
+                    </div>
+                    <div class="part-1-answer">
+                        <input type="radio" name="part-2-answer-${questionKey} value="D"  onclick="onChangeAnswerPart2(${questionKey}, 'D')">
+                        D. ${answerD}.
+                    </div>
+                </div>
+            </div>`
+        examPart2.innerHTML += questionBox
+  
+        userAnswer[questionKey] = ''
+    })
+  
+    Object.keys(part3).forEach((questionKey) => {
+        const question = part3[questionKey]
+        const {topic, answerA, answerB, answerC, answerD, correctAnswer} = question
+        const questionBox = 
+            `<div class="part-1-question">
+            <div class="part-1-topic">Câu ${questionKey}: ${topic}</div>
+            <div class="part-1-answers">
+                <div class="part-1-answer">
+                    <input type="checkbox" onclick="onChangeAnswerPart3(${questionKey} , 'A')">
+                    A. ${answerA}
+                </div>
+                <div class="part-1-answer" onclick="onChangeAnswerPart3(${questionKey}, 'B')">
+                    <input type="checkbox">
+                    B. ${answerB}
+                </div>
+                <div class="part-1-answer">
+                    <input type="checkbox" onclick="onChangeAnswerPart3(${questionKey} ,'C')">
+                    C. ${answerC}
+                </div>
+                <div class="part-1-answer" onclick="onChangeAnswerPart3(${questionKey}, 'D')">
+                    <input type="checkbox">
+                    D. ${answerD}
+                </div>
+            </div>
+        </div>`
+        examPart3.innerHTML += questionBox
+  
+        userAnswer[questionKey] = []
+    })
+  
+    Object.keys(part4).forEach((questionKey) => {
+        const question = part4[questionKey]
+        const {topic, answerA, answerB, answerC, answerD, correctAnswer} = question
+        const questionBox = 
+            `<div class="part-1-question">
+                <div class="part-1-topic">Câu ${questionKey}: ${topic}</div>
+                <div class="part-1-answers">
+                    <div class="part-1-answer">
+                        <input type="text" name="part-1-answer" onchange="onChangeAnswerPart4(${questionKey})" id="question-${questionKey}">
+                    </div>
+                    
+                </div>
+            </div>`
+        examPart4.innerHTML += questionBox
+  
+        userAnswer[questionKey] = ''
+    })
+  
+    console.log(userAnswer)
+  }
+  
+
+  const loadResultPage = () => {
+    let correctAnswer = 0
+    for(let i = 1; i <= 40; i++) {
+        if(userAnswer[i] === result[i]) {
+            correctAnswer += 1
+        }
+    }
+    document.getElementById('result-scores').innerText = `${correctAnswer}/40`
+  }
+  
+  const onChangeAnswerPart2 = (questionKey, answer) => {
+    userAnswer[questionKey] = answer
+    console.log(userAnswer)
+  }
+  
+  const onChangeAnswerPart3 = (questionKey, answer) => {
+    userAnswer[questionKey].includes(answer) ? userAnswer[questionKey].filter((item) => item === answer) : userAnswer[questionKey].push(answer)
+    console.log(userAnswer)
+  }
+  
+  
+  
+  const onChangeAnswerPart4 = (questionKey) => {
+    const question_id = `question-${questionKey}`
+    userAnswer[questionKey] = document.getElementById(question_id).value
+    console.log(userAnswer)
+  }
+
+  
